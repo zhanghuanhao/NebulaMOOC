c78beb40583603c4a525ec6a449bd89911a9f673
Garlan666 <1572279033@qq.com>
2019-03-28 21:01:49 +0800

    123

--- live/src/main/java/com/nebula/mooc/live/ServerApplication.java
+++ live/src/main/java/com/nebula/mooc/live/ServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(ServerApplication.class, args);
+        //SpringApplication.run(ServerApplication.class, args);1
     }
 
 }
