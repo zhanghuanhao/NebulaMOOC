bd6269e085e9bc44736788264fd512acd7b20ddf
Cheung <254892464@qq.com>
2019-04-04 20:12:05 +0800

    Add / redirect to /index.html

--- src/main/java/com/nebula/mooc/controller/StandardController.java
+++ src/main/java/com/nebula/mooc/controller/StandardController.java
@@ -6,13 +6,14 @@ package com.nebula.mooc.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class StandardController {
 
-    @RequestMapping(value = "/h")
-    public String index() {
-        return "redirect:/index.html";
+    @RequestMapping(value = "test")
+    public ModelAndView index() {
+        return new ModelAndView(new RedirectView("index.html"));
     }
-
 }
