e9750fae0681b38c542561393aa20379ba2220bd
Cheung <254892464@qq.com>
2019-04-10 22:50:17 +0800

    test

--- web-server/src/main/java/com/nebula/mooc/webserver/controller/StandardController.java
+++ web-server/src/main/java/com/nebula/mooc/webserver/controller/StandardController.java
@@ -7,16 +7,17 @@ package com.nebula.mooc.webserver.controller;
 import com.nebula.mooc.webserver.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.annotation.Resource;
 
 @RestController
 public class StandardController {
 
-//    @GetMapping(value = "get")
-//    public ModelAndView index() {
-//        return new ModelAndView(new RedirectView("index.html"));
-//    }
+    @GetMapping(value = "get")
+    public RedirectView index() {
+        return new RedirectView("index.html");
+    }
 
     @Resource
     TestService testService;
