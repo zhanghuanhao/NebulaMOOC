edd3aa2c142dd699a2316eb1ddadab8295938aa5
Cheung <254892464@qq.com>
2019-04-10 15:48:59 +0800

    Reconstruct

--- web-server/src/main/java/com/nebula/mooc/controller/StandardController.java
+++ web-server/src/main/java/com/nebula/mooc/controller/StandardController.java
@@ -5,7 +5,7 @@
 package com.nebula.mooc.controller;
 
 import com.nebula.mooc.service.TestService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -13,15 +13,15 @@ import javax.annotation.Resource;
 @RestController
 public class StandardController {
 
-    //    @RequestMapping(value = "getIndex")
+//    @GetMapping(value = "get")
 //    public ModelAndView index() {
 //        return new ModelAndView(new RedirectView("index.html"));
 //    }
-//
+
     @Resource
     TestService testService;
 
-    @RequestMapping(value = "getNickName")
+    @GetMapping(value = "getNickName")
     public String count() {
         return testService.getNickName();
     }
