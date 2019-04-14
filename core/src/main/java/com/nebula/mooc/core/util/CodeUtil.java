a111aac9ec212f8dcdd578bfc5c51f37330ff326
Cheung <254892464@qq.com>
2019-04-14 23:38:29 +0800

    Add coreUtil

---
+++ core/src/main/java/com/nebula/mooc/core/util/CodeUtil.java
@@ -0,0 +1,88 @@
+package com.nebula.mooc.core.util;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+/**
+ * Created by 15722 on 2019/4/14.
+ */
+public class CodeUtil {
+    // 图片的宽度。
+    private static final int width = 100;
+    // 图片的高度。
+    private static final int height = 36;
+    // 验证码字符个数
+    private static final int codeCount = 5;
+    // 验证码干扰线数
+    private static final int lineCount = 50;
+
+    // 验证码范围,去掉0(数字)和O(拼音)容易混淆的(小写的1和L也可以去掉,大写不用了)
+    private static final char[] codeSequence = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'L', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+
+    public static String createCode() {
+        // randomCode记录随机产生的验证码
+        StringBuilder randomCode = new StringBuilder();
+        // 生成随机数
+        Random random = new Random();
+        // 随机产生codeCount个字符的验证码。
+        for (int i = 0; i < codeCount; i++) {
+            String strRand = String.valueOf(codeSequence[random.nextInt(codeSequence.length)]);
+            // 将产生的5个随机数组合在一起。
+            randomCode.append(strRand);
+        }
+        return randomCode.toString();
+    }
+
+    public static BufferedImage createImgCode(String code) {
+        int x = 0, fontHeight = 0, codeY = 0;
+        int red = 0, green = 0, blue = 0;
+
+        x = width / (codeCount + 2);//每个字符的宽度(左右各空出一个字符)
+        fontHeight = height - 2;//字体的高度
+        codeY = height - 4;
+
+        // 图像buffer
+        BufferedImage buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = buffImg.createGraphics();
+        // 生成随机数
+        Random random = new Random();
+        // 将图像填充为白色
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        // 创建字体,可以修改为其它的
+        Font font = new Font("Fixedsys", Font.PLAIN, fontHeight);
+
+        g.setFont(font);
+
+        for (int i = 0; i < lineCount; i++) {
+            // 设置随机开始和结束坐标
+            int xs = random.nextInt(width);//x坐标开始
+            int ys = random.nextInt(height);//y坐标开始
+            int xe = xs + random.nextInt(width / 8);//x坐标结束
+            int ye = ys + random.nextInt(height / 8);//y坐标结束
+
+            // 产生随机的颜色值，让输出的每个干扰线的颜色值都将不同。
+            red = random.nextInt(255);
+            green = random.nextInt(255);
+            blue = random.nextInt(255);
+            g.setColor(new Color(red, green, blue));
+            g.drawLine(xs, ys, xe, ye);
+        }
+
+        // 随机产生codeCount个字符的验证码。
+        for (int i = 0; i < codeCount; i++) {
+            // 产生随机的颜色值，让输出的每个字符的颜色值都将不同。
+            red = random.nextInt(255);
+            green = random.nextInt(255);
+            blue = random.nextInt(255);
+            g.setColor(new Color(red, green, blue));
+            g.drawString(code.substring(i, i + 1), (i + 1) * x, codeY);
+        }
+        return buffImg;
+    }
+
+}
---
+++ web-server/src/main/resources/static/js/server.js
@@ -0,0 +1,134 @@
+﻿var cooldown = 60;//获取邮件验证码倒计时
+
+//Ajax提交
+function AjaxPost(Url, JsonData, LodingFun, ReturnFun) {
+    $.ajax({
+        type: "post",
+        url: Url,
+        data: JsonData,
+        dataType: 'json',
+        async: 'false',
+        beforeSend: LodingFun,
+        error: function () {
+            AjaxErro({"Status": "Erro", "Erro": "500"});
+        },
+        success: ReturnFun
+    });
+}
+
+//弹出提示框
+function ErroAlert(e) {
+    layer.msg(e, {
+        icon: 5,
+        time: 2000,
+        skin: 'my-skin'
+    });
+}
+
+//弹出确认框
+function msgAlert(e) {
+    layer.msg(e, {
+        icon: 1,
+        time: 2000,
+        skin: 'my-skin'
+    });
+}
+
+//Ajax 错误返回处理
+function AjaxErro(e) {
+    if (e.Status == "Erro") {
+        switch (e.Erro) {
+            case "500":
+                top.location.href = '/Erro/Erro500';
+                break;
+            case "100001":
+                ErroAlert("错误 : 错误代码 '10001'");
+                break;
+            default:
+                ErroAlert(e.Erro);
+        }
+    } else {
+        layer.msg("未知错误！");
+    }
+}
+
+
+/* 发送用户识别的图片验证码*/
+function Login(JSONdata, ReturnFun) {
+    $.ajax({
+        type: "POST",
+        url: "/sys/user/login",
+        contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+        data: JSONdata,
+        dataType: 'json',
+        success: ReturnFun,
+        error: function () {
+            ErroAlert('获取图片验证码失败');
+        }
+    });
+}
+
+/*请求邮件验证码*/
+function getemail(SussessFun) {
+
+    var JSONdata = {address: $('.username').val()};
+    $.ajax({
+        type: "POST",
+        url: "/sys/code/getMailCode",
+        contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+        data: JSONdata,
+        dataType: 'json',
+        success: SussessFun,
+        error: function (e) {
+            ErroAlert('发送失败');
+            alert(e);
+        }
+    });
+}
+
+/*60秒倒计时*/
+function cooltime(o) {
+    if (cooldown == 0) {
+        o.removeAttribute("disabled");
+        o.value = "获取验证码";
+    } else {
+        o.setAttribute("disabled", true);
+        o.value = "重新发送(" + cooldown + ")";
+        cooldown--;
+        setTimeout(function () {
+            cooltime(o);
+        }, 1000);
+    }
+}
+
+/*发送用户收到的邮箱验证码*/
+function register(JSONdata, ReturnFun) {
+    $.ajax({
+        type: "POST",
+        url: "",
+        contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+        data: JSONdata,
+        dataType: 'json',
+        success: ReturnFun,
+        error: function () {
+            ErroAlert('请获取验证码');
+        }
+    });
+}
+
+/*找回密码*/
+function findPwd(JSONdata, ReturnFun) {
+    $.ajax({
+        type: "POST",
+        url: "",
+        contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+        data: JSONdata,
+        dataType: 'json',
+        success: ReturnFun,
+        error: function () {
+            ErroAlert('请获取验证码');
+        }
+    });
+
+}
+
