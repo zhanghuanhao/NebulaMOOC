3b7f0bd963a9fa51b459d61a9c9892ff778896b2
Cheung <254892464@qq.com>
2019-04-15 15:24:24 +0800

    Add welcome page

--- web-server/src/main/java/com/nebula/mooc/webserver/config/LoginConfig.java
+++ web-server/src/main/java/com/nebula/mooc/webserver/config/LoginConfig.java
@@ -8,12 +8,14 @@ import com.nebula.mooc.core.entity.Constant;
 import com.nebula.mooc.core.util.CookieUtil;
 import com.nebula.mooc.webserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -37,36 +39,45 @@ public class LoginConfig extends WebMvcConfigurationSupport implements HandlerIn
     }
 
     /**
+     * 添加html为可以被解析为视图
+     */
+    @Bean
+    public InternalResourceViewResolver htmlViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    /**
+     * 绑定默认欢迎页
+     * 添加url : / -> view : index
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+
+    /**
      * 添加拦截器
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(this).addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/js/**", "/res/**",
-                        "/plugins/**", "/sys/**", "/error/**");
-    }
-
-    /**
-     * 绑定默认欢迎页
-     */
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/index.html").setViewName("index"); //添加index.html为view -> index
-        registry.addViewController("/").setViewName("index");
+                        "/plugins/**", "/sys/**", "/error/**",
+                        Constant.LOGIN_PATH);
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         System.out.println(request.getServletPath());
-        if (!Constant.LOGIN_PATH.equals(request.getServletPath())) {
-            String token = CookieUtil.get(request, Constant.TOKEN);
-            boolean result = token != null && userService.loginCheck(token);
-            if (!result) {
-                response.sendRedirect(Constant.LOGIN_PATH);
-                return false;
-            }
+        String token = CookieUtil.get(request, Constant.TOKEN);
+        boolean result = token != null && userService.loginCheck(token);
+        if (!result) {
+            response.sendRedirect(Constant.LOGIN_PATH);
+            return false;
         }
         return true;
     }
-
 }
