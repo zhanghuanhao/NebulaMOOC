4b0f0fdcb3288cac1d364c453ec29d997efe482c
Cheung <254892464@qq.com>
2019-04-16 10:59:28 +0800

    Add error controller

---
+++ web-server/src/main/java/com/nebula/mooc/webserver/controller/ErrorHandler.java
@@ -0,0 +1,35 @@
+/*
+ * @author Zhanghh
+ * @date 2019/4/15
+ */
+package com.nebula.mooc.webserver.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 全局错误处理器
+ */
+@Controller
+public class ErrorHandler implements ErrorController {
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        //获取statusCode:404,500等
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == 404)
+            return "/err/404";
+        else if (statusCode == 500)
+            return "/err/500";
+        else
+            return "/err/default";
+    }
+}
