841941dec978a800a97f3500223195e583e874c3
Cheung <254892464@qq.com>
2019-04-23 23:34:49 +0800

    Add protobuf support

--- chat-server/src/main/java/com/nebula/mooc/chatserver/config/MyChannelInitializer.java
+++ chat-server/src/main/java/com/nebula/mooc/chatserver/config/MyChannelInitializer.java
@@ -5,11 +5,16 @@
 package com.nebula.mooc.chatserver.config;
 
 import com.nebula.mooc.chatserver.handler.MyWebSocketHandler;
+import com.nebula.mooc.core.entity.ChatMessage;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class MyChannelInitializer<C extends Channel> extends ChannelInitializer<C> {
@@ -25,12 +30,12 @@ public class MyChannelInitializer<C extends Channel> extends ChannelInitializer<
                 .addLast(new ChunkedWriteHandler())
                 // protobufDecoder仅仅负责编码，并不支持读半包，所以在之前，一定要有读半包的处理器。
                 // 半包的处理
-//                    .addLast(new ProtobufVarint32FrameDecoder())
-//                    // 需要解码的目标类
-//                    .addLast(new ProtobufDecoder(PersonProbuf.Person.getDefaultInstance()))
-//                    .addLast(new ProtobufVarint32LengthFieldPrepender())
-//                    .addLast(new ProtobufEncoder())
-//                    .addLast(new MyWebSocketHandler());
+                .addLast(new ProtobufVarint32FrameDecoder())
+                // 需要解码的目标类
+                .addLast(new ProtobufDecoder(ChatMessage.request.getDefaultInstance()))
+                .addLast(new ProtobufVarint32LengthFieldPrepender())
+                .addLast(new ProtobufEncoder())
+                .addLast(new MyWebSocketHandler())
                 //用于处理websocket, /ws为访问websocket时的uri
                 .addLast(new WebSocketServerProtocolHandler("/"))
                 .addLast(new MyWebSocketHandler());
