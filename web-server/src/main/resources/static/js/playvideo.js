4f37dda6248bb204350b409de84d604013b1faab
happydeldy <2048772100@qq.com>
2019-06-06 18:51:17 +0800

    Add index

--- web-server/src/main/resources/static/js/playvideo.js
+++ web-server/src/main/resources/static/js/playvideo.js
@@ -137,7 +137,8 @@ function webSocketConnect() {
 //让弹幕动起来
 var moveObj = function (obj, Color) {
     var topMax = $("#showWords").height();
-    var _top = Math.floor(topMax * (Math.random())); //设置top初始位置为面板高度内的随机数
+    var fontPosition = $("#fontposition option selected").val();
+    var _top = Math.floor(1 / 3 * topMax * (Math.random() + fontPosition)); //设置top初始位置为面板高度内的随机数
     if (_top + obj.height() >= topMax) {
         _top -= obj.height();
     }
