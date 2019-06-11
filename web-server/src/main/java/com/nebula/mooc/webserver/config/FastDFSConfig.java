b6a126703d772fac42ea6a80b300322a9ebad9b6
Cheung <254892464@qq.com>
2019-06-11 11:43:23 +0800

    Fixed

--- web-server/src/main/java/com/nebula/mooc/webserver/config/FastDFSConfig.java
+++ web-server/src/main/java/com/nebula/mooc/webserver/config/FastDFSConfig.java
@@ -7,41 +7,67 @@ package com.nebula.mooc.webserver.config;
 import org.csource.fastdfs.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.net.InetSocketAddress;
 
 @Configuration
 public class FastDFSConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(FastDFSConfig.class);
 
-    @Autowired
-    private TrackerServer trackerServer;
+    @Value("${fdfs.connectTimeout}")
+    private int connectTimeout;
 
-    @Bean
-    public TrackerServer trackerServer() {
-        TrackerServer trackerServer = null;
+    @Value("${fdfs.networkTimeout}")
+    private int networkTimeout;
+
+    @Value("${fdfs.charset}")
+    private String charset;
+
+    @Value("${fdfs.trackerServerAddress}")
+    private String trackerServerAddress;
+
+    private TrackerGroup getTrackerGroup() throws Exception {
+        String[] trackerServers = trackerServerAddress.split(",");
+        InetSocketAddress[] tracker_servers = new InetSocketAddress[trackerServers.length];
+        for (int i = 0; i < trackerServers.length; i++) {
+            String[] parts = trackerServers[i].split(":", 2);
+            if (parts.length != 2) {
+                throw new Exception("the value of item \"trackerServerAddress\" is invalid, the correct format is host:port");
+            }
+            tracker_servers[i] = new InetSocketAddress(parts[0].trim(), Integer.parseInt(parts[1].trim()));
+        }
+        return new TrackerGroup(tracker_servers);
+    }
+
+    @PostConstruct
+    public void init() {
         try {
-            ClientGlobal.init("fdfs_client.conf");
-            TrackerClient trackerClient = new TrackerClient(ClientGlobal.g_tracker_group);
-            trackerServer = trackerClient.getConnection();
+            ClientGlobal.setG_connect_timeout(connectTimeout);
+            ClientGlobal.setG_network_timeout(networkTimeout);
+            ClientGlobal.setG_charset(charset);
+            ClientGlobal.setG_tracker_group(getTrackerGroup());
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
-        logger.info("FastDFS - Tracker inited");
-        return trackerServer;
+        logger.info("FastDFS Config inited");
     }
 
     @Bean
     public StorageClient imageClient() {
-        StorageClient imageClient = null;
+        StorageClient imageClient;
         try {
             TrackerClient trackerClient = new TrackerClient(ClientGlobal.g_tracker_group);
+            TrackerServer trackerServer = trackerClient.getConnection();
             StorageServer storageServer = trackerClient.getStoreStorage(trackerServer, "image");
             imageClient = new StorageClient(trackerServer, storageServer);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
+            return null;
         }
         logger.info("FastDFS - Image Storage inited");
         return imageClient;
@@ -49,13 +75,15 @@ public class FastDFSConfig {
 
     @Bean
     public StorageClient videoClient() {
-        StorageClient videoClient = null;
+        StorageClient videoClient;
         try {
             TrackerClient trackerClient = new TrackerClient(ClientGlobal.g_tracker_group);
+            TrackerServer trackerServer = trackerClient.getConnection();
             StorageServer storageServer = trackerClient.getStoreStorage(trackerServer, "video");
             videoClient = new StorageClient(trackerServer, storageServer);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
+            return null;
         }
         logger.info("FastDFS - Video Storage inited");
         return videoClient;
